f0dc58a5a85183ae804faa291cee7ae795d9fa35
Murad Ahmed Tahmim <muradahmedtahmim@gmail.com>
2026-02-25 23:32:19 +0600

    first init

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("My First App"),
+          backgroundColor: Colors.deepPurple[500],
+          elevation: 0,
+          leading: Icon(Icons.menu),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
+        ),
+        // 39.22
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              // curve the corners a bit
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: EdgeInsets.all(25),
+            // child: Text("Murad Ahmed",
+            //   style: TextStyle(
+            //     color: Colors.white,
+            //     fontSize: 28,
+            //     fontWeight: FontWeight.bold,
+            //   ),
+            // ),
+            child:Icon(Icons.favorite,color: Colors.white,),
+          ),
+        ),
+      ),
+    );
+  }
+}
